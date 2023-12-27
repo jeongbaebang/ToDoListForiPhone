e84dae419e364257907d5e603673a22599dc06cd
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-12-27 12:11:15 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by jeongbae bang on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
