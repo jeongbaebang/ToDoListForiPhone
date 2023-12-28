ff6128947c374fe9776bd27c2648a68ca0b6f7ba
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-12-28 17:09:09 +0900

    create: LoginView

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var eamil = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                
+                // Login Form
+                Form {
+                    TextField("이메일", text: $eamil)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .textContentType(.emailAddress)
+                    
+                    SecureField("비밀번호", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // Attempt log in
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundStyle(.blue)
+                            
+                            Text("Log In")
+                                .foregroundStyle(.white)
+                                .bold()
+                        }
+                    }
+                    .padding(.vertical)
+                }
+                
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                    
+                    NavigationLink("Create An Account", destination: RegisterVIew())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
