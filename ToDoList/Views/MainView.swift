1c716133f13eaad594d21e49abafcb0dde08f73e
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-12-29 15:16:17 +0900

    create: tabview 항목 추가

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject var viewModel = MainViewViewModel()
+    
     var body: some View {
         NavigationView {
             if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-                ToDoListView()
+                accountView()
             } else {
                 LoginView()
             }
+        }
+    }
+    
+    @ViewBuilder
+    func accountView() -> some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     var body: some View {
-        Text("Welcome to your account!")
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("To Do List")
+        }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "")
 }
