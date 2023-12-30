fdfc786caa7dc7165c5c0d7c2f57e799b9f39fa3
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2023-12-30 18:52:29 +0900

    create: 로그아웃 & 마이페이지

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,17 +13,56 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
-            }
-            .navigationTitle("Profile")
-            .toolbar {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "plus")
+                if let user = viewModel.user {
+                    profile(user)
+                } else {
+                    Text("Loading Profile...")
                 }
             }
+            .navigationTitle("Profile")
         }
+        .onAppear(perform: {
+            viewModel.fetchUser()
+        })
+    }
+    
+    @ViewBuilder
+    func profile(_ user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundStyle(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            
+            HStack {
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+        }
+        .padding()
+        
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        
+        Spacer()
     }
 }
 
